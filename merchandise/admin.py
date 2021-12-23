43493082721072ee1be28c2e4ee7cc92a0825333
hartnetl <laura.hartnett@outlook.com>
2021-12-23 13:25:52 +0000

    Feat: Create urls for products - merch and donations

--- merchandise/admin.py
+++ merchandise/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Category, Merch, Donate
 
+
 class MerchAdmin(admin.ModelAdmin):
     list_display = (
         'name',
--- merchandise/views.py
+++ merchandise/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Merch, Donate
 
+
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     merchandise = Merch.objects.all()
     donations = Donate.objects.all()
-    
+
     context = {
         'merchandise': merchandise,
         'donations': donations,
