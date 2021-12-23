cc34834a529e965edc7dda3c36d5348df4fe19cd
hartnetl <laura.hartnett@outlook.com>
2021-12-23 13:16:37 +0000

    Delete: Remove fixtures

--- merchandise/admin.py
+++ merchandise/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Category, Merch, Donate
 
-# Register your models here.
+class MerchAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('rating',)
+
+
+class DonateAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+    )
+
+    ordering = ('price',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Merch, MerchAdmin)
+admin.site.register(Donate, DonateAdmin)
