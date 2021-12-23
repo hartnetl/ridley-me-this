7d1d7350ae0ae9272b5ef3bbb9c9839e899571fb
hartnetl <laura.hartnett@outlook.com>
2021-12-23 13:22:40 +0000

    Feat: Create view for merch and donations

--- merchandise/views.py
+++ merchandise/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Merch, Donate
 
-# Create your views here.
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    merchandise = Merch.objects.all()
+    donations = Donate.objects.all()
+    
+    context = {
+        'merchandise': merchandise,
+        'donations': donations,
+    }
+
+    return render(request, 'merchandise/merch.html', context)
