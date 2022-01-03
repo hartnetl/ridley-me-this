f688640cc0788a70b9b688ed2e1dc21457b59b88
hartnetl <laura.hartnett@outlook.com>
2022-01-03 13:26:49 +0000

    Feat: Add category filter for merchandise and donations

--- merchandise/views.py
+++ merchandise/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, reverse
-from .models import Merch, Donate
+from .models import Merch, Donate, Category
 
 
 def all_merch(request):
@@ -7,10 +7,20 @@ def all_merch(request):
 
     merchandise = Merch.objects.all()
     donations = Donate.objects.all()
+    categories = None
+
+    if request.GET:
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            merchandise = merchandise.filter(category__name__in=categories)
+            donations = donations.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'merchandise': merchandise,
         'donations': donations,
+        'current_categories': categories,
     }
 
     return render(request, 'merchandise/merchandise.html', context)
