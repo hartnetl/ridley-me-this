41b7e859207f40e419a2cf454e18f1051154ae85
hartnetl <laura.hartnett@outlook.com>
2022-01-07 11:05:48 +0000

    Feat: Add empty learn more page with working view, url and template

--- info/urls.py
+++ info/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.about_page, name='info'),
+    path('learn/', views.learn_more, name='learn'),
 ]
--- info/views.py
+++ info/views.py
@@ -7,3 +7,10 @@ def about_page(request):
     template = 'info/info.html'
 
     return render(request, template)
+
+def learn_more(request):
+    """ A view to show page where users can learn more about eco living, turtles etc """
+
+    template = 'info/learn.html'
+
+    return render(request, template)
