907950cf1062a83407f127dd626542f1bc3d0786
hartnetl <laura.hartnett@outlook.com>
2022-01-07 18:04:31 +0000

    Feat: Add contact to admin

--- contact/views.py
+++ contact/views.py
@@ -3,6 +3,11 @@ from .forms import ContactForm
 
 def contact(request):
     """ A view to show contact page with contact form """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'contact/success.html')
     form = ContactForm()
     context = {'form': form}
     return render(request, 'contact/contact.html', context)
