77d9c28a2f1b27503c51b89dc8bae4d10710d199
hartnetl <laura.hartnett@outlook.com>
2022-01-08 17:02:54 +0000

    Chore: Make some fixes to admin panel and turtle card
    Fix turtle admin views to change the list display. Change country on turtle card to display the country name.

--- turtles/admin.py
+++ turtles/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from .models import Turtles, turtle_sponsor
 
-admin.site.register(Turtles)
-admin.site.register(turtle_sponsor)
 
 class TurtlesAdmin(admin.ModelAdmin):
     list_display = (
-        'title'
+        'title',
         'name',
-        'speies',
+        'species',
         'turtle_id',
         'date_tagged',
         'location_tagged',
@@ -27,4 +25,5 @@ class TurtleSponsor(admin.ModelAdmin):
     )
 
 
-
+admin.site.register(Turtles, TurtlesAdmin)
+admin.site.register(turtle_sponsor, TurtleSponsor)
