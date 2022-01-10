6cb8a24417ad2f18dd22c26aeefd5561731007f5
hartnetl <laura.hartnett@outlook.com>
2022-01-10 16:33:28 +0000

    Feat: Add add-to-bag functionality to turtle

--- basket/views.py
+++ basket/views.py
@@ -13,7 +13,8 @@ def view_basket(request):
 def add_merch_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping basket """
 
-    merch = get_object_or_404(Merch, pk=item_id) 
+    merch = get_object_or_404(Merch, pk=item_id)
+    turtle = get_object_or_404(Turtle, pk=item_id)
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -22,10 +23,10 @@ def add_merch_to_basket(request, item_id):
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
-        messages.success(request, f'Updated {merch.name} quantity to {basket[item_id]}')
+        messages.success(request, f'Updated merch.name  quantity to {basket[item_id]}')
     else:
         basket[item_id] = quantity
-        messages.success(request, f'Added {merch.name} to your basket')
+        messages.success(request, f'Added merch.name to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
